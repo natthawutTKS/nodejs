1e5475dd83e97d26e770bcecd9b5f0db51f248c4
natthawut_sae <natthawut.sae@ascendcorp.com>
2023-09-06 12:02:11 +0700

    cypess

---
+++ cypress/e2e/error.cy.js
@@ -0,0 +1,11 @@
+describe('Health Check API Test', () => {
+    it('Should return a 200 status code', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:3000', // Replace with the actual health check URL
+        failOnStatusCode: false, // Do not fail the test on non-200 status codes
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+      });
+    })
+  })
